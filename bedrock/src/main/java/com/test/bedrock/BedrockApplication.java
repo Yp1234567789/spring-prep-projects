abb5e217e757ba02b171fe748a6eb5b481438f6b
avulreddypaturu14 <avulreddy09@gmail.com>
2026-02-28 13:21:51 -0500

    Initial clean commit

---
+++ bedrock/src/main/java/com/test/bedrock/BedrockApplication.java
@@ -0,0 +1,13 @@
+package com.test.openai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BedrockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenaiApplication.class, args);
+	}
+
+}
