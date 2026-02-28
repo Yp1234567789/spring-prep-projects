a08bd478e272a83bcddfa95046afa5e8552c7ff8
avulreddypaturu14 <avulreddy09@gmail.com>
2026-02-28 13:37:43 -0500

    Initial clean commit

---
+++ multimodel/src/main/java/com/test/multimodel/controller/MultiModelChatController.java
@@ -0,0 +1,34 @@
+package com.test.openai.controller;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class MultiModelChatController  {
+
+    private final ChatClient openAiChatClient;
+    private final ChatClient bedrockChatClient;
+
+    public MultiModelChatController(@Qualifier("openAiChatClient") ChatClient openAiChatClient,
+                                    @Qualifier("bedrockChatClient") ChatClient bedrockChatClient) {
+        this.openAiChatClient = openAiChatClient;
+        this.bedrockChatClient = bedrockChatClient;
+    }
+
+
+    @GetMapping("/openai/chat")
+    public String openAIChat(@RequestParam("message") String message) {
+
+        return openAiChatClient.prompt(message).call().content();
+    }
+    @GetMapping("/openai/chat")
+    public String bedrockAIChat(@RequestParam("message") String message) {
+
+        return bedrockChatClient.prompt(message).call().content();
+    }
+
+}
