cba8eb26af49b5e77effc2200f3fba9a8894adfd
avulreddypaturu14 <avulreddy09@gmail.com>
2026-03-05 14:20:19 -0500

    mcp serverstdio and mcpserver remote  intial check ins

---
+++ mcpserverstdio/src/main/java/com/test/mcpserverremote/repository/HelpDeskTicketRepository.java
@@ -0,0 +1,12 @@
+package com.test.mcpserverremote.repository;
+
+import com.test.mcpserverremote.entity.HelpDeskTicket;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface HelpDeskTicketRepository extends JpaRepository<HelpDeskTicket, Long> {
+
+    List<HelpDeskTicket> findByUsername(String username);
+
+}
